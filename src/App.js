68819ff2046ebad6ad858ef45f954d68fc356ecd
OlenaTka4uk <bushueva07@gmail.com>
2024-10-20 12:06:58 -0400

    add inst_cons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/LandingPage';
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
 
 function App() {
   return (
@@ -10,9 +11,10 @@ function App() {
       <BrowserRouter>
           
           <Navbar/>
-         
+         <InstantConsultation/>
           <Routes>
             <Route path='/' element={<Landing_Page/>}></Route>
+            <Route path="/instant-consultation" element={<InstantConsultation />} />
           </Routes>
         </BrowserRouter>
     </div>
